dad181fd77182a629cfccc74da4aed0979190e10
Myles Selim <Selim042@users.noreply.github.com>
2018-04-09 11:50:47 -0500

    Version bump

--- src/main/java/selim/modjam/packs/ModJamPacks.java
+++ src/main/java/selim/modjam/packs/ModJamPacks.java
@@ -48,7 +48,7 @@ public class ModJamPacks {
 
 	public static final String MODID = "selimpacks";
 	public static final String NAME = "Selim's Backpacks";
-	public static final String VERSION = "1.1.2";
+	public static final String VERSION = "1.1.3";
 	public static final ResourceLocation CAPABILITY_ID = new ResourceLocation(MODID, "backpack");
 	@Mod.Instance(value = MODID)
 	public static ModJamPacks instance;
